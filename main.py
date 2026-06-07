1649169f544c5e95e59fe2c5e50309bfa6ec0051
Remaster-Expert-1829 <yashdeep.singhjsr@gmail.com>
2026-06-07 22:00:49 +0530

    initial version working now

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 #from curl_cffi import requests             also doesn't work wow
 #from playwright.sync_api import sync_playwright     I am gonna cry broooo
 #from playwright_stealth import Stealth              even big dawg not working :crying emoji:
-from seleniumbase import SB
+#from seleniumbase import SB                         I am crying ngl
+from playwright.sync_api import sync_playwright
 import time
 import html
 import json
@@ -32,79 +33,87 @@ url= "https://old.reddit.com/r/developersindia/search.json?q=resume+OR+graduate&
 def main():
     print("Launching browser instance to fetch data...")
     #with Stealth().use_sync(sync_playwright()) as p:
-    with SB(uc=True,headless=False) as sb:
-        #browser=p.chromium.launch(headless=False)
-        #telling playwright to use a normal chrome user agent to avoid it explicitly announcing itself as a bot
-        '''context=browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-        )'''
-        #page=browser.new_page()
-        #page=context.new_page()
-        #stealth_sync(page)
-        #response=page.goto(url)
-        sb.driver.get(url)
-        
-        print(f"Browser navigated to the page.")
-        #delay incase Cloudflare has an invisible challenge to process, thus giving it some time to complete before trying to access the content
-        time.sleep(3)
-        #raw_text=page.locator("body").inner_text()
-        raw_text=sb.get_text("body")
-        if "You've been blocked by network security" in raw_text:
-            print("Error 403: we doomed")
-        elif "Too Many Requests" in raw_text:
-            print("Error: 429: getting rate limited. Please try again later")
-        else:
-            try:
-                #data=response.json()
-                data=json.loads(raw_text)
+    #with SB(uc=True,headless=False) as sb:
+    with sync_playwright() as p:
+        try:
+            browser=p.chromium.connect_over_cdp("http://localhost:9222")
+            #browser=p.chromium.launch(headless=False)
+            #telling playwright to use a normal chrome user agent to avoid it explicitly announcing itself as a bot
+            '''context=browser.new_context(
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+            )'''
+            default_context=browser.contexts[0]
+            page=default_context.pages[0]
+            #page=browser.new_page()
+            #page=context.new_page()
+            #stealth_sync(page)
+            response=page.goto(url)
+            #sb.driver.get(url)
+            
+            print(f"Browser navigated to the page.")
+            #delay incase Cloudflare has an invisible challenge to process, thus giving it some time to complete before trying to access the content
+            time.sleep(3)
+            raw_text=page.locator("body").inner_text()
+            #raw_text=sb.get_text("body")
+            if "You've been blocked by network security" in raw_text:
+                print("Error 403: we doomed")
+            elif "Too Many Requests" in raw_text:
+                print("Error: 429: getting rate limited. Please try again later")
+            else:
+                try:
+                    #data=response.json()
+                    data=json.loads(raw_text)
 
-                valid_post_count=0
+                    valid_post_count=0
 
-                for post in data['data']['children']:
-                    post_data=post['data']
-                    #skipping posts that are older than 5 years
-                    if post_data['created_utc'] < five_years_ago:
-                        continue
-                    title=post_data['title']
-                    permalink=f"https://www.reddit.com{post_data['permalink']}"
-                    #for storing all image urls from a post
-                    image_urls = []
-                    #getting image urls from a gallery post
-                    if post_data.get('is_gallery'):
-                        media_dict=post_data.get('media_metadata',{})
-                        
-                        for media_id,media_info in media_dict.items():
-                            if media_info['e']=='Image':
-                                raw_url=media_info['s'].get('u', '')
-                                clean_url=html.unescape(raw_url)
-                                image_urls.append(clean_url)
-                    else:
-                        post_url=post_data.get('url', '')
-                        if post_url.endswith(('.jpg',',jpeg','.png')) or 'i.redd.it' in post_url:
-                            image_urls.append(post_url)
-                    if image_urls:
-                        valid_post_count+=1
-                        print(f"[{valid_post_count}] Title: {title}")
-                        print(f"Post link: {permalink}")
-                        for count,img in enumerate(image_urls,1):
-                            print(f" -> Image {count}: {img}")
-                        print("-"*60)
-                print(f"\nFound {valid_post_count} valid image posts within the last 5 years")
-            #except requests.exceptions.JSONDecodeError:         sorri
-            except json.JSONDecodeError:
-                print("Failed to parse JSON response")
-                #page.screenshot(path="/debug/reddit_block_screenshot.png")
-                sb.save_screenshot("/debug/reddit_block_screenshot.png")
-                print("Saved a screenshot of Reddit for debugging help")
-                #print("Raw Response content snippet:")
-                #print(response.text[:500])
-        '''if response.status !=200:
-            print(f"Error fetching data: Reddit returned status code {response.status}")
-            if response.status == 429:
-                print("Getting rate limited. Please try again later")
-            elif response.status == 403:
-                print("Access forbidden. Reddit is blocking this User-agent")''' #this no more for old guys as selenium kinda like a human so it is
-                #hard to read HTTP status codes as it opens the website and doesn't return any response object like requests does, so we have to check
-                #the page content for any error messages instead
+                    for post in data['data']['children']:
+                        post_data=post['data']
+                        #skipping posts that are older than 5 years
+                        if post_data['created_utc'] < five_years_ago:
+                            continue
+                        title=post_data['title']
+                        permalink=f"https://www.reddit.com{post_data['permalink']}"
+                        #for storing all image urls from a post
+                        image_urls = []
+                        #getting image urls from a gallery post
+                        if post_data.get('is_gallery'):
+                            media_dict=post_data.get('media_metadata',{})
+                            
+                            for media_id,media_info in media_dict.items():
+                                if media_info['e']=='Image':
+                                    raw_url=media_info['s'].get('u', '')
+                                    clean_url=html.unescape(raw_url)
+                                    image_urls.append(clean_url)
+                        else:
+                            post_url=post_data.get('url', '')
+                            if post_url.endswith(('.jpg',',jpeg','.png')) or 'i.redd.it' in post_url:
+                                image_urls.append(post_url)
+                        if image_urls:
+                            valid_post_count+=1
+                            print(f"[{valid_post_count}] Title: {title}")
+                            print(f"Post link: {permalink}")
+                            for count,img in enumerate(image_urls,1):
+                                print(f" -> Image {count}: {img}")
+                            print("-"*60)
+                    print(f"\nFound {valid_post_count} valid image posts within the last 5 years")
+                #except requests.exceptions.JSONDecodeError:         sorri
+                except json.JSONDecodeError:
+                    print("Failed to parse JSON response")
+                    page.screenshot(path="/debug/reddit_block_screenshot.png")
+                    #sb.save_screenshot("/debug/reddit_block_screenshot.png")
+                    print("Saved a screenshot of Reddit for debugging help")
+                    #print("Raw Response content snippet:")
+                    #print(response.text[:500])
+            '''if response.status !=200:
+                print(f"Error fetching data: Reddit returned status code {response.status}")
+                if response.status == 429:
+                    print("Getting rate limited. Please try again later")
+                elif response.status == 403:
+                    print("Access forbidden. Reddit is blocking this User-agent")''' #this no more for old guys as selenium kinda like a human so it is
+                    #hard to read HTTP status codes as it opens the website and doesn't return any response object like requests does, so we have to check
+                    #the page content for any error messages instead
+        except Exception as e:
+            print(f"Failed to connect to Chrome: {e}")
+            print("Make sure you ran the 'chrome.exe --remote-debugging-port=9222 --user-data-dir='C:\\ChromeDebug'' command first!")
 if __name__ == "__main__":
     main()
